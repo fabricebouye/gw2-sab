93b881447665e7fc5b59ff2cf38a2954ccca1dee
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-03-12 17:17:18 +1100

    Added some more guild logs renders.

--- gw2-sab/src/com/bouye/gw2/sab/scene/guild/log/LogEventListCell.java
+++ gw2-sab/src/com/bouye/gw2/sab/scene/guild/log/LogEventListCell.java
@@ -7,9 +7,12 @@
  */
 package com.bouye.gw2.sab.scene.guild.log;
 
+import api.web.gw2.mapping.core.CoinAmount;
 import api.web.gw2.mapping.v2.guild.id.log.LogEvent;
+import api.web.gw2.mapping.v2.guild.id.log.LogEventStashOperation;
 import java.time.ZonedDateTime;
 import java.util.Optional;
+import java.util.OptionalInt;
 import javafx.scene.control.ListCell;
 
 /**
@@ -25,18 +28,18 @@ public final class LogEventListCell extends ListCell<LogEvent> {
     }
 
     @Override
-    protected void updateItem(final LogEvent item, final boolean empty) {
-        super.updateItem(item, empty);
+    protected void updateItem(final LogEvent logEvent, final boolean empty) {
+        super.updateItem(logEvent, empty);
         String text = null;
         boolean wrapText = false;
-        if (!empty && item != null) {
-            final ZonedDateTime time = item.getTime();
-            final String user = item.getUser();
-            switch (item.getType()) {
+        if (!empty && logEvent != null) {
+            final ZonedDateTime time = logEvent.getTime();
+            final String user = logEvent.getUser();
+            switch (logEvent.getType()) {
                 case INFLUENCE:
                     break;
                 case INVITED: {
-                    final Optional<String> invitedBy = item.getInvitedBy();
+                    final Optional<String> invitedBy = logEvent.getInvitedBy();
                     final String inviter = invitedBy.isPresent() ? invitedBy.get() : "";
                     text = String.format("%s - %s has invited %s into the guild.", time.toString(), inviter, user);
                 }
@@ -50,7 +53,7 @@ public final class LogEventListCell extends ListCell<LogEvent> {
                 }
                 break;
                 case KICK: {
-                    final Optional<String> kickedBy = item.getKickedBy();
+                    final Optional<String> kickedBy = logEvent.getKickedBy();
                     final String kicker = kickedBy.isPresent() ? kickedBy.get() : "";
                     if (user.equals(kicker)) {
                         text = String.format("%s - %s decided to quit the guild.", time.toString(), user);
@@ -60,26 +63,50 @@ public final class LogEventListCell extends ListCell<LogEvent> {
                 }
                 break;
                 case MOTD: {
-                    final Optional<String> motd = item.getMotd();
+                    final Optional<String> motd = logEvent.getMotd();
                     final String message = motd.isPresent() ? motd.get() : "";
                     text = String.format("%s - %s has edited the message of the day:\n%s.", time.toString(), user, message);
                     wrapText = true;
                 }
                 break;
                 case RANK_CHANGE: {
-                    final Optional<String> newRank = item.getNewRank();
+                    final Optional<String> newRank = logEvent.getNewRank();
                     final String promotion = newRank.isPresent() ? newRank.get() : "";
-                    final Optional<String> changeBy = item.getChangedBy();
+                    final Optional<String> changeBy = logEvent.getChangedBy();
                     final String promoter = changeBy.isPresent() ? changeBy.get() : "";
                     text = String.format("%s - %s has decided that %s will now be %s.", time.toString(), promoter, user, promotion);
                 }
                 break;
-                case STASH:
-                    break;
-                case TREASURY:
-                    break;
-                case UPGRADE:
-                    break;
+                case STASH: {
+                    final Optional<LogEventStashOperation> operation = logEvent.getOperation();
+                    final String op = operation.isPresent() ? ((operation.get() == LogEventStashOperation.DEPOSIT) ? "deposited" : "withdrawn") : "";
+                    final OptionalInt itemId = logEvent.getItemId();
+                    if (itemId.isPresent()) {
+                        final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : "";
+                        final OptionalInt count = logEvent.getCount();
+                        final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : "";
+                        text = String.format("%s - %s has %s %s x %s in the guild's stash.", time.toString(), user, op, number, item);
+                    } else {
+                        final Optional<CoinAmount> coins = logEvent.getCoins();
+                        final String currency = coins.isPresent() ? coins.get().toString() : "";
+                        text = String.format("%s - %s has %s %s in the guild's stash.", time.toString(), op, user, currency);
+                    }
+                }
+                break;
+                case TREASURY: {
+                    final OptionalInt itemId = logEvent.getItemId();
+                    final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : "";
+                    final OptionalInt count = logEvent.getCount();
+                    final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : "";
+                    text = String.format("%s - %s has deposited %s x %s in the guild's treasury.", time.toString(), user, number, item);
+                }
+                break;
+                case UPGRADE: {
+                    final OptionalInt upgradeId = logEvent.getUpgradeId();
+                    final String upgrade = upgradeId.isPresent() ? String.valueOf(upgradeId.getAsInt()) : "";
+                    text = String.format("%s - %s has planned upgrade %s.", time.toString(), user, upgrade);
+                }
+                break;
                 default:
             }
         }
