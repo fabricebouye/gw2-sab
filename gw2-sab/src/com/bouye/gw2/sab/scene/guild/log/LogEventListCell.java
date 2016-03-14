003735273d394282e89678d8513b16588e0d1eb1
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-03-14 19:46:06 +1100

    Externalized text to resource file. The list cell is now presented similar to the one ingame.

--- gw2-sab/src/com/bouye/gw2/sab/scene/guild/log/LogEventListCell.java
+++ gw2-sab/src/com/bouye/gw2/sab/scene/guild/log/LogEventListCell.java
@@ -7,12 +7,16 @@
  */
 package com.bouye.gw2.sab.scene.guild.log;
 
-import api.web.gw2.mapping.core.CoinAmount;
 import api.web.gw2.mapping.v2.guild.id.log.LogEvent;
-import api.web.gw2.mapping.v2.guild.id.log.LogEventStashOperation;
-import java.time.ZonedDateTime;
+import com.bouye.gw2.sab.SAB;
+import com.bouye.gw2.sab.SABConstants;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 
 /**
@@ -20,97 +24,31 @@ import javafx.scene.control.ListCell;
  * @author Fabrice Bouyé
  */
 public final class LogEventListCell extends ListCell<LogEvent> {
+    
+    private Optional<Node> node = Optional.empty();
+    private Optional<LogEventListCellController> controller = Optional.empty();
 
     /**
      * Creates a new instance.
      */
     public LogEventListCell() {
+        try {
+            final String fxml = "fxml/scene/guild/log/LogEventListCell.fxml";
+            final URL fxmlURL = SAB.class.getResource(fxml);
+            final FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL, SABConstants.I18N);
+            node = Optional.of(fxmlLoader.load());
+            controller = Optional.of(fxmlLoader.getController());
+        } catch (IOException ex) {
+            Logger.getLogger(LogEventListCell.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
-
+    
     @Override
     protected void updateItem(final LogEvent logEvent, final boolean empty) {
         super.updateItem(logEvent, empty);
-        String text = null;
-        boolean wrapText = false;
-        if (!empty && logEvent != null) {
-            final ZonedDateTime time = logEvent.getTime();
-            final String user = logEvent.getUser();
-            switch (logEvent.getType()) {
-                case INFLUENCE:
-                    break;
-                case INVITED: {
-                    final Optional<String> invitedBy = logEvent.getInvitedBy();
-                    final String inviter = invitedBy.isPresent() ? invitedBy.get() : "";
-                    text = String.format("%s - %s has invited %s into the guild.", time.toString(), inviter, user);
-                }
-                break;
-                case INVITE_DECLINED: {
-                    text = String.format("%s - %s declined to join the guild.", time.toString(), user);
-                }
-                break;
-                case JOINED: {
-                    text = String.format("%s - %s has joined the guild.", time.toString(), user);
-                }
-                break;
-                case KICK: {
-                    final Optional<String> kickedBy = logEvent.getKickedBy();
-                    final String kicker = kickedBy.isPresent() ? kickedBy.get() : "";
-                    if (user.equals(kicker)) {
-                        text = String.format("%s - %s decided to quit the guild.", time.toString(), user);
-                    } else {
-                        text = String.format("%s - %s kicked %s from the guild.", time.toString(), kicker, user);
-                    }
-                }
-                break;
-                case MOTD: {
-                    final Optional<String> motd = logEvent.getMotd();
-                    final String message = motd.isPresent() ? motd.get() : "";
-                    text = String.format("%s - %s has edited the message of the day:\n%s.", time.toString(), user, message);
-                    wrapText = true;
-                }
-                break;
-                case RANK_CHANGE: {
-                    final Optional<String> newRank = logEvent.getNewRank();
-                    final String promotion = newRank.isPresent() ? newRank.get() : "";
-                    final Optional<String> changeBy = logEvent.getChangedBy();
-                    final String promoter = changeBy.isPresent() ? changeBy.get() : "";
-                    text = String.format("%s - %s has decided that %s will now be %s.", time.toString(), promoter, user, promotion);
-                }
-                break;
-                case STASH: {
-                    final Optional<LogEventStashOperation> operation = logEvent.getOperation();
-                    final String op = operation.isPresent() ? ((operation.get() == LogEventStashOperation.DEPOSIT) ? "deposited" : "withdrawn") : "";
-                    final OptionalInt itemId = logEvent.getItemId();
-                    if (itemId.isPresent()) {
-                        final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : "";
-                        final OptionalInt count = logEvent.getCount();
-                        final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : "";
-                        text = String.format("%s - %s has %s %s x %s in the guild's stash.", time.toString(), user, op, number, item);
-                    } else {
-                        final Optional<CoinAmount> coins = logEvent.getCoins();
-                        final String currency = coins.isPresent() ? coins.get().toString() : "";
-                        text = String.format("%s - %s has %s %s in the guild's stash.", time.toString(), op, user, currency);
-                    }
-                }
-                break;
-                case TREASURY: {
-                    final OptionalInt itemId = logEvent.getItemId();
-                    final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : "";
-                    final OptionalInt count = logEvent.getCount();
-                    final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : "";
-                    text = String.format("%s - %s has deposited %s x %s in the guild's treasury.", time.toString(), user, number, item);
-                }
-                break;
-                case UPGRADE: {
-                    final OptionalInt upgradeId = logEvent.getUpgradeId();
-                    final String upgrade = upgradeId.isPresent() ? String.valueOf(upgradeId.getAsInt()) : "";
-                    text = String.format("%s - %s has planned upgrade %s.", time.toString(), user, upgrade);
-                }
-                break;
-                default:
-            }
-        }
-        setText(text);
-        setWrapText(wrapText);
+        setText(null);
+        controller.ifPresent(c -> c.setLogEvent(logEvent));
+        final Node graphic = (empty || logEvent == null || !node.isPresent() ? null : node.get());
+        setGraphic(graphic);
     }
 }
---
+++ gw2-sab/src/com/bouye/gw2/sab/scene/guild/log/LogEventListCellController.java
@@ -0,0 +1,181 @@
+/* 
+ * Copyright (C) 2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package com.bouye.gw2.sab.scene.guild.log;
+
+import api.web.gw2.mapping.core.CoinAmount;
+import api.web.gw2.mapping.v2.guild.id.log.LogEvent;
+import api.web.gw2.mapping.v2.guild.id.log.LogEventInfluenceActivity;
+import api.web.gw2.mapping.v2.guild.id.log.LogEventStashOperation;
+import com.bouye.gw2.sab.SABConstants;
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.ResourceBundle;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ * @author Fabrice Bouyé
+ */
+public final class LogEventListCellController implements Initializable {
+
+    @FXML
+    private Label textLabel;
+    @FXML
+    private Label dateLabel;
+
+    @Override
+    public void initialize(final URL url, final ResourceBundle rb) {
+        logEvent.addListener(logEventInvalidationListener);
+    }
+
+    /**
+     * Called whenever the log event is invalidated.
+     */
+    private final InvalidationListener logEventInvalidationListener = observable -> updateContent();
+
+    private void updateContent() {
+        // Clear old content.
+        textLabel.setText(null);
+        dateLabel.setText(null);
+        // Install new content.
+        final Optional<LogEvent> logEvent = Optional.ofNullable(getLogEvent());
+        logEvent.ifPresent(this::installContent);
+    }
+
+    /**
+     * Install new content.
+     * @param logEvent The log event.
+     */
+    private void installContent(final LogEvent logEvent) {
+        // @todo In game time is presented as a diff from "now".
+        final ZonedDateTime time = logEvent.getTime();
+        dateLabel.setText(time.toString());
+        final String user = logEvent.getUser();
+        String text = null;
+        // @todo We need to be able to resolve item ids to get localized names and rarity color.
+        switch (logEvent.getType()) {
+            case INFLUENCE:
+                // @todo influence events return by the Web API seem incomplete or buggy.
+                final Optional<LogEventInfluenceActivity> activity = logEvent.getActivity();
+                switch (activity.get()) {
+                    case DAILY_LOGIN:
+                        text = SABConstants.I18N.getString("guild-log.influence.daily-login.label"); // NOI18N.
+                        break;
+                    case GIFTED:
+                        text = SABConstants.I18N.getString("guild-log.influence.gifted.label"); // NOI18N.
+                        break;
+                    default:
+                }
+                break;
+            case INVITED: {
+                final Optional<String> invitedBy = logEvent.getInvitedBy();
+                final String inviter = invitedBy.isPresent() ? invitedBy.get() : ""; // NOI18N.
+                text = SABConstants.I18N.getString("guild-log.invited.label"); // NOI18N.
+                text = String.format(text, inviter, user);
+            }
+            break;
+            case INVITE_DECLINED: {
+                text = SABConstants.I18N.getString("guild-log.declined.label"); // NOI18N.
+                text = String.format(text, user);
+            }
+            break;
+            case JOINED: {
+                text = SABConstants.I18N.getString("guild-log.joined.label"); // NOI18N.
+                text = String.format(text, user);
+            }
+            break;
+            case KICK: {
+                final Optional<String> kickedBy = logEvent.getKickedBy();
+                final String kicker = kickedBy.isPresent() ? kickedBy.get() : ""; // NOI18N.
+                if (user.equals(kicker)) {
+                    text = SABConstants.I18N.getString("guild-log.kick-self.label"); // NOI18N.
+                    text = String.format(text, user);
+                } else {
+                    text = SABConstants.I18N.getString("guild-log.kick.label"); // NOI18N.
+                    text = String.format(text, kicker, user);
+                }
+            }
+            break;
+            case MOTD: {
+                final Optional<String> motd = logEvent.getMotd();
+                final String message = motd.isPresent() ? motd.get() : ""; // NOI18N.
+                text = message;
+            }
+            break;
+            case RANK_CHANGE: {
+                final Optional<String> newRank = logEvent.getNewRank();
+                final String promotion = newRank.isPresent() ? newRank.get() : ""; // NOI18N.
+                final Optional<String> changeBy = logEvent.getChangedBy();
+                final String promoter = changeBy.isPresent() ? changeBy.get() : ""; // NOI18N.
+                text = SABConstants.I18N.getString("guild-log.rank-change.label"); // NOI18N.
+                text = String.format(text, promoter, user, promotion);
+            }
+            break;
+            case STASH: {
+                final Optional<LogEventStashOperation> operation = logEvent.getOperation();
+                final String op = operation.isPresent() ? ((operation.get() == LogEventStashOperation.DEPOSIT) ? "deposited" : "withdrewn") : "";
+                final OptionalInt itemId = logEvent.getItemId();
+                if (itemId.isPresent()) {
+                    final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : ""; // NOI18N.
+                    final OptionalInt count = logEvent.getCount();
+                    final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : ""; // NOI18N.
+                    text = SABConstants.I18N.getString("guild-log.stash.object.label"); // NOI18N.
+                    text = String.format(text, user, op, item, number);
+                } else {
+                    final Optional<CoinAmount> coins = logEvent.getCoins();
+                    final String currency = coins.isPresent() ? coins.get().toString() : ""; // NOI18N.
+                    text = SABConstants.I18N.getString("guild-log.stash.coin.label"); // NOI18N.
+                    text = String.format(text, op, user, currency);
+                }
+            }
+            break;
+            case TREASURY: {
+                final OptionalInt itemId = logEvent.getItemId();
+                final String item = itemId.isPresent() ? String.valueOf(itemId.getAsInt()) : ""; // NOI18N.
+                final OptionalInt count = logEvent.getCount();
+                final String number = count.isPresent() ? String.valueOf(count.getAsInt()) : ""; // NOI18N.
+                text = SABConstants.I18N.getString("guild-log.treasury.label"); // NOI18N.
+                text = String.format(text, user, item, number);
+            }
+            break;
+            case UPGRADE: {
+                final OptionalInt upgradeId = logEvent.getUpgradeId();
+                final String upgrade = upgradeId.isPresent() ? String.valueOf(upgradeId.getAsInt()) : ""; // NOI18N.
+                text = SABConstants.I18N.getString("guild-log.upgrade.label"); // NOI18N.
+                text = String.format(text, user, upgrade);
+            }
+            break;
+            default:
+        }
+        textLabel.setText(text);
+    }
+
+    /**
+     * The log event to display.
+     */
+    private final ObjectProperty<LogEvent> logEvent = new SimpleObjectProperty(this, "logEvent", null); // NOI18N.
+
+    public final LogEvent getLogEvent() {
+        return logEvent.getValue();
+    }
+
+    public final void setLogEvent(final LogEvent value) {
+        logEvent.setValue(value);
+    }
+
+    public final ObjectProperty<LogEvent> logEventProperty() {
+        return logEvent;
+    }
+}
