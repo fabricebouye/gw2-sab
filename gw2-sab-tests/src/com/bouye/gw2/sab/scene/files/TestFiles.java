aa6b982c363c35e2ceb005fd41855cf08d687961
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-04-16 17:20:52 +1100

    Added a test for v2/files queries and images stored in db storage.

--- gw2-sab-tests/src/com/bouye/gw2/sab/scene/files/TestFiles.java
+++ gw2-sab-tests/src/com/bouye/gw2/sab/scene/files/TestFiles.java
@@ -9,18 +9,25 @@ package com.bouye.gw2.sab.scene.files;
 
 import api.web.gw2.mapping.core.JsonpContext;
 import api.web.gw2.mapping.v2.files.File;
+import com.bouye.gw2.sab.db.DBStorage;
 import com.bouye.gw2.sab.query.ImageCache;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -31,31 +38,62 @@ import javafx.stage.Stage;
 public class TestFiles extends Application {
 
     @Override
+    public void init() throws Exception {
+        DBStorage.INSTANCE.init();
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
         final String query = "https://api.guildwars2.com/v2/files?ids=all";
         final Collection<File> files = JsonpContext.SAX.loadObjectArray(File.class, new URL(query));
-        final FlowPane flowPane = new FlowPane();
-        flowPane.getChildren()
-                .setAll(files.stream().
-                        map(file -> {
-                            final Label label = new Label(file.getId());
-                            final VBox result = new VBox();
-                            result.setPadding(new Insets(10));
-                            result.setSpacing(6);
-                            final Image image = ImageCache.INSTANCE.getImage(file.getIcon().get().toExternalForm());
-                            final ImageView imageView = new ImageView(image);
-                            result.getChildren().setAll(label, imageView);
-                            return result;
-                        })
-                        .collect(Collectors.toList()));
+        final HBox hbox = new HBox();
+        // Gets files directly from the web api.
+        {
+            final FlowPane flowPane = new FlowPane();
+            flowPane.getChildren()
+                    .setAll(files.stream()
+                            .map(file -> {
+                                final Image image = ImageCache.INSTANCE.getImage(file.getIcon().get().toExternalForm());
+                                return nodeForFile(file.getId(), image);
+                            })
+                            .collect(Collectors.toList()));
+            final VBox vbox = new VBox();
+            vbox.getChildren().setAll(new Label("Remote"), flowPane);
+            HBox.setHgrow(vbox, Priority.ALWAYS);
+            hbox.getChildren().add(vbox);
+        }
+        // Gets files stored in the local DB.
+        {
+            final FlowPane flowPane = new FlowPane();
+            flowPane.getChildren()
+                    .setAll(files.stream()
+                            .map(file -> {
+                                final Image image = ImageCache.INSTANCE.getImage(file.getId());
+                                return nodeForFile(file.getId(), image);
+                            })
+                            .collect(Collectors.toList()));
+            final VBox vbox = new VBox();
+            vbox.getChildren().setAll(new Label("Local"), flowPane);
+            HBox.setHgrow(vbox, Priority.ALWAYS);
+            hbox.getChildren().add(vbox);
+        }
         final ScrollPane root = new ScrollPane();
         root.setFitToWidth(true);
-        root.setContent(flowPane);
-        Scene scene = new Scene(root, 600, 600);
-
+        root.setContent(hbox);
+        final Scene scene = new Scene(root, 600, 600);
         primaryStage.setTitle("TestFiles");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private Node nodeForFile(final String id, final Image image) {
+        final Label label = new Label(id);
+        final ImageView imageView = new ImageView(image);
+        final VBox result = new VBox();
+        result.setPadding(new Insets(10));
+        result.setSpacing(6);
+        result.getChildren().setAll(label, imageView);
+        return result;
     }
 
     /**
