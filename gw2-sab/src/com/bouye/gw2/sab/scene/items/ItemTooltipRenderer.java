740c4fde6edefa1924df37f5b56a7a8d7801907a
Fabrice Bouyé <fabriceb@spc.int>
2016-05-02 15:50:28 +1100

    introduced a wrapper class for items.

--- gw2-sab/src/com/bouye/gw2/sab/scene/items/ItemTooltipRenderer.java
+++ gw2-sab/src/com/bouye/gw2/sab/scene/items/ItemTooltipRenderer.java
@@ -9,8 +9,13 @@ package com.bouye.gw2.sab.scene.items;
 
 import com.bouye.gw2.sab.SAB;
 import com.bouye.gw2.sab.scene.SABFXMLUtils;
+import com.bouye.gw2.sab.wrappers.ItemWrapper;
 import java.net.URL;
 import java.util.Optional;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Group;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -38,5 +43,27 @@ public final class ItemTooltipRenderer extends GridPane {
     public String getUserAgentStylesheet() {
         final URL url = SAB.class.getResource("styles/scene/items/ItemTooltipRenderer.css"); // NOI18N.
         return url.toExternalForm();
+    }
+
+    private final ObjectProperty<ItemWrapper> item = new SimpleObjectProperty<>(this, "item", null); // NOI18N.
+
+    public final ItemWrapper getItem() {
+        return item.get();
+    }
+
+    public final void setItem(final ItemWrapper value) {
+        item.set(value);
+    }
+
+    public final ObjectProperty<ItemWrapper> itemProperty() {
+        return item;
+    }
+
+    public static Tooltip asTooltip(final ItemWrapper item) {
+        final ItemTooltipRenderer renderer = new ItemTooltipRenderer();
+        renderer.setItem(item);
+        final Tooltip result = new Tooltip();
+        result.setGraphic(new Group(renderer));
+        return result;
     }
 }
---
+++ gw2-sab/src/com/bouye/gw2/sab/wrappers/ItemWrapper.java
@@ -0,0 +1,42 @@
+/* 
+ * Copyright (C) 2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package com.bouye.gw2.sab.wrappers;
+
+import api.web.gw2.mapping.v2.items.Item;
+import api.web.gw2.mapping.v2.skins.Skin;
+import java.util.Objects;
+
+/**
+ * Wraps an item.
+ * @author Fabrice Bouyé
+ */
+public final class ItemWrapper {
+
+    private final Item item;
+    private final Skin skin;
+
+    /**
+     * Creates a new instance.
+     * @param item The item.
+     * @param skin The skin, may be {@code null}.
+     * @throws NullPointerException If {@code item} is {@code null}.
+     */
+    public ItemWrapper(final Item item, final Skin skin) throws NullPointerException {
+        Objects.requireNonNull(item);
+        this.item = item;
+        this.skin = skin;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public Skin getStkin() {
+        return skin;
+    }
+}
