d15e004c14936e13c60c14012c5eb710c344b922
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-05-04 19:31:36 +1100

    Added missing cell renderer for profession tests.

---
+++ gw2-sab-tests/src/com/bouye/gw2/sab/scene/characters/professions/SimpleProfessionListCell.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package com.bouye.gw2.sab.scene.characters.professions;
+
+import api.web.gw2.mapping.v2.professions.Profession;
+import javafx.scene.control.ListCell;
+
+/**
+ * Test.
+ * @author Fabrice Bouyé
+ */
+final class SimpleProfessionListCell extends ListCell<Profession> {
+
+    /**
+     * Creates a new instance.
+     */
+    public SimpleProfessionListCell() {
+    }
+
+    @Override
+    protected void updateItem(final Profession item, final boolean empty) {
+        super.updateItem(item, empty);
+        final String text = (empty || item == null) ? null : item.getName();
+        setText(text);
+    }
+}
