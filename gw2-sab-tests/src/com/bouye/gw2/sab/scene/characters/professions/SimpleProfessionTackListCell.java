3b02d52e32354df18c1d7445e059756d1b6b539a
Fabrice Bouyé <fabrice.bouye@gmail.com>
2016-05-05 10:53:58 +1100

    User can now select profession and track from test UI.

---
+++ gw2-sab-tests/src/com/bouye/gw2/sab/scene/characters/professions/SimpleProfessionTackListCell.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package com.bouye.gw2.sab.scene.characters.professions;
+
+import api.web.gw2.mapping.v2.professions.ProfessionTrack;
+import javafx.scene.control.ListCell;
+
+/**
+ * Test.
+ * @author Fabrice Bouyé
+ */
+final class SimpleProfessionTackListCell extends ListCell<ProfessionTrack> {
+
+    /**
+     * Creates a new instance.
+     */
+    public SimpleProfessionTackListCell() {
+    }
+
+    @Override
+    protected void updateItem(final ProfessionTrack item, final boolean empty) {
+        super.updateItem(item, empty);
+        final String text = (empty || item == null) ? null : item.getName();
+        setText(text);
+    }
+}
--- gw2-sab-tests/src/com/bouye/gw2/sab/scene/characters/professions/TestProfessionTrackEditor.java
+++ gw2-sab-tests/src/com/bouye/gw2/sab/scene/characters/professions/TestProfessionTrackEditor.java
@@ -9,10 +9,12 @@ package com.bouye.gw2.sab.scene.characters.professions;
 
 import api.web.gw2.mapping.core.JsonpContext;
 import api.web.gw2.mapping.v2.professions.Profession;
+import api.web.gw2.mapping.v2.professions.ProfessionTrack;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -20,7 +22,8 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.scenicview.ScenicView;
 
@@ -36,9 +39,25 @@ public final class TestProfessionTrackEditor extends Application {
         final ComboBox<Profession> professionCombo = new ComboBox<>();
         professionCombo.setButtonCell(new SimpleProfessionListCell());
         professionCombo.setCellFactory(listView -> new SimpleProfessionListCell());
-        professionCombo.valueProperty().addListener((observable, oldValue, newValue) -> professionEditor.setProfession(newValue));
-        final StackPane root = new StackPane();
-        root.getChildren().add(professionEditor);
+        final ComboBox<ProfessionTrack> trackCombo = new ComboBox<>();
+        trackCombo.setButtonCell(new SimpleProfessionTackListCell());
+        trackCombo.setCellFactory(listView -> new SimpleProfessionTackListCell());
+        professionCombo.valueProperty().addListener((observable, oldValue, newValue) -> {
+            final Set<ProfessionTrack> tracks = (newValue == null) ? Collections.EMPTY_SET : newValue.getTraining();
+            trackCombo.getItems().setAll(tracks);
+            if (!tracks.isEmpty()) {
+                trackCombo.getSelectionModel().select(0);
+            }
+            professionEditor.setProfession(newValue);
+        });
+        trackCombo.valueProperty().addListener((observable, oldValue, newValue) -> {
+            professionEditor.setTrack(newValue);
+        });
+        final ToolBar toolBar = new ToolBar();
+        toolBar.getItems().addAll(professionCombo, trackCombo);
+        final BorderPane root = new BorderPane();
+        root.setTop(toolBar);
+        root.setCenter(professionEditor);
         final Scene scene = new Scene(root, 600, 600);
         primaryStage.setTitle("TestProfessionTrackEditor"); // NOI18N.
         primaryStage.setScene(scene);
