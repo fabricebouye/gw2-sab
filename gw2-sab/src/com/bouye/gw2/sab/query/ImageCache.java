9a62ab35495e52bcd4b7d2812b05ec8bedce0454
Fabrice Bouyé <fabriceb@spc.int>
2016-05-13 09:18:02 +1100

    Reeinforce the need for non-null parameter.

--- gw2-sab/src/com/bouye/gw2/sab/query/ImageCache.java
+++ gw2-sab/src/com/bouye/gw2/sab/query/ImageCache.java
@@ -13,6 +13,7 @@ import java.lang.ref.SoftReference;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.image.Image;
@@ -38,9 +39,10 @@ public enum ImageCache {
      * Retrieves an image from the cache.
      * <br>If the image is not in the cache, an empty image is returned and the data of the image is loaded in the background.
      * @param url The URL of the image.
-     * @return An {@code Image} instance, may be {@code null} if {@code url} is {@code null}, empty or invalid.
+     * @return An {@code Image} instance, may be {@code null}.
+     * @throws NullPointerException If {@code url} is {@code null}.
      */
-    public Image getImage(final String url) {
+    public Image getImage(final String url) throws NullPointerException {
         return getImage(url, true);
     }
 
@@ -48,33 +50,33 @@ public enum ImageCache {
      * Retrieves an image from the cache.
      * @param url The URL of the image.
      * @param backgroundLoading If {@code true}, the image is loaded in background.
-     * @return An {@code Image} instance, may be {@code null} if {@code url} is {@code null}, empty or invalid.
+     * @return An {@code Image} instance, may be {@code null}.
+     * @throws NullPointerException If {@code url} is {@code null}.
      */
-    public Image getImage(final String url, final boolean backgroundLoading) {
+    public Image getImage(final String url, final boolean backgroundLoading) throws NullPointerException {
+        Objects.requireNonNull(url);
         Image result = null;
-        if (url != null && !url.trim().isEmpty()) {
-            synchronized (cache) {
-                SoftReference<Image> imageRef = cache.get(url);
-                result = (imageRef == null) ? null : imageRef.get();
-                if (result == null && !cache.containsKey(url)) {
-                    try {
-                        // For local images, access the data base and ignore the background loading parameter.
-                        if (!url.startsWith("https://")) { // NOI18N.
-                            result = DBStorage.INSTANCE.getImageFromCache(url);
-                        } // Remote image.
-                        else if (!SABConstants.INSTANCE.isOffline()) {
-                            result = new Image(url, backgroundLoading);
-                        } else {
-                            // @todo Return a local (as embededed within the app) default non-null image when in offline mode.   
-                        }
-                        imageRef = new SoftReference<>(result);
-                    } catch (Exception ex) {
-                        Logger.getLogger(getClass().getName()).log(Level.WARNING, ex.getMessage(), ex);
-                        cache.put(url, null);
+        synchronized (cache) {
+            SoftReference<Image> imageRef = cache.get(url);
+            result = (imageRef == null) ? null : imageRef.get();
+            if (result == null && !cache.containsKey(url)) {
+                try {
+                    // For local images, access the data base and ignore the background loading parameter.
+                    if (!url.startsWith("https://")) { // NOI18N.
+                        result = DBStorage.INSTANCE.getImageFromCache(url);
+                    } // Remote image.
+                    else if (!SABConstants.INSTANCE.isOffline()) {
+                        result = new Image(url, backgroundLoading);
+                    } else {
+                        // @todo Return a local (as embededed within the app) default non-null image when in offline mode.   
                     }
-                    // Storing the key upon failure should prevent from further retrieval attempts.
-                    cache.put(url, imageRef);
+                    imageRef = new SoftReference<>(result);
+                } catch (Exception ex) {
+                    Logger.getLogger(getClass().getName()).log(Level.WARNING, ex.getMessage(), ex);
+                    cache.put(url, null);
                 }
+                // Storing the key upon failure should prevent from further retrieval attempts.
+                cache.put(url, imageRef);
             }
         }
         return result;
