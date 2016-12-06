fe6cff850e67c879f09ceff6f89af086082b0473
Fabrice Bouyé <fabriceb@spc.int>
2016-12-07 10:59:37 +1100

    Expanded test.

--- gw2-sab-tests/src/com/bouye/gw2/sab/scene/items/TestItemTooltipRenderer.java
+++ gw2-sab-tests/src/com/bouye/gw2/sab/scene/items/TestItemTooltipRenderer.java
@@ -8,10 +8,12 @@
 package com.bouye.gw2.sab.scene.items;
 
 import api.web.gw2.mapping.core.JsonpContext;
+import api.web.gw2.mapping.core.URLReference;
 import api.web.gw2.mapping.v2.items.Item;
 import com.bouye.gw2.sab.SAB;
 import com.bouye.gw2.sab.SABConstants;
-import com.bouye.gw2.sab.query.WebQuery;
+import com.bouye.gw2.sab.query.GW2APIClient;
+import com.bouye.gw2.sab.query.ImageCache;
 import com.bouye.gw2.sab.wrappers.ItemWrapper;
 import java.io.IOException;
 import java.net.URL;
@@ -21,16 +23,21 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import javafx.application.Application;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 import org.scenicview.ScenicView;
 
@@ -40,17 +47,25 @@ import org.scenicview.ScenicView;
  */
 public final class TestItemTooltipRenderer extends Application {
 
-    private VBox vbox;
+    private GridPane gridPane;
 
     @Override
     public void start(final Stage primaryStage) {
         // Left pane: all renderers.
-        vbox = new VBox();
-        vbox.setFillWidth(true);
+        gridPane = new GridPane();
+        gridPane.setHgap(6);
+        gridPane.setVgap(6);
+        gridPane.getColumnConstraints().setAll(IntStream.rangeClosed(0, 1)
+                .mapToObj(index -> {
+                    final ColumnConstraints columnConstraints = new ColumnConstraints();
+                    columnConstraints.setHgrow(Priority.ALWAYS);
+                    return columnConstraints;
+                })
+                .toArray(ColumnConstraints[]::new));
         final ScrollPane scrollPane = new ScrollPane();
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
-        scrollPane.setContent(vbox);
+        scrollPane.setContent(gridPane);
         //
         final SplitPane splitPane = new SplitPane();
         splitPane.getItems().setAll(scrollPane);
@@ -62,63 +77,102 @@ public final class TestItemTooltipRenderer extends Application {
         primaryStage.setTitle("TestItemTooltipRenderer"); // NOI18N.
         primaryStage.setScene(scene);
         primaryStage.show();
-//        Platform.runLater(() -> splitPane.setDividerPositions(0.33, 0.66));
-//        ScenicView.show(root);
+        ScenicView.show(root);
         loadTestAsync();
     }
 
-    private void loadTestAsync() {
-        Service<List<ItemWrapper>> service = new Service<List<ItemWrapper>>() {
-            @Override
-            protected Task<List<ItemWrapper>> createTask() {
-                return new Task<List<ItemWrapper>>() {
-                    @Override
-                    protected List<ItemWrapper> call() throws Exception {
-                        List<ItemWrapper> result = SABConstants.INSTANCE.isOffline() ? loadLocalTest() : loadRemoteTest();
-                        return result;
-                    }
-                };
-            }
-        };
-        service.setOnSucceeded(workerStateEvent -> {
-            final List<ItemWrapper> items = (List<ItemWrapper>) workerStateEvent.getSource().getValue();
-            vbox.getChildren().setAll(items
-                    .stream()
-                    .map(item -> {
-                        final ItemTooltipRenderer renderer1 = new ItemTooltipRenderer();
-                        renderer1.setItem(item);
-                        final Tooltip tooltip = ItemTooltipRenderer.asTooltip(item);
-                        Tooltip.install(renderer1, tooltip);
-                        return renderer1;
-                    })
-                    .collect(Collectors.toList()));
+    private Service loadService;
 
-        });
-        service.setOnFailed(workerStateEvent -> {
-            final Throwable ex = workerStateEvent.getSource().getException();
-            Logger.getGlobal().log(Level.SEVERE, ex.getMessage(), ex);
-        });
-        service.start();
+    private void loadTestAsync() {
+        if (loadService == null) {
+            final Service<List<ItemWrapper>> service = new Service<List<ItemWrapper>>() {
+                @Override
+                protected Task<List<ItemWrapper>> createTask() {
+                    return new Task<List<ItemWrapper>>() {
+                        @Override
+                        protected List<ItemWrapper> call() throws Exception {
+                            final List<ItemWrapper> result = SABConstants.INSTANCE.isOffline() ? loadLocalTest() : loadRemoteTest();
+                            return result;
+                        }
+                    };
+                }
+            };
+            service.setOnSucceeded(workerStateEvent -> {
+                final List<ItemWrapper> items = (List<ItemWrapper>) workerStateEvent.getSource().getValue();
+                populateUI(items);
+            });
+            service.setOnFailed(workerStateEvent -> {
+                final Throwable ex = workerStateEvent.getSource().getException();
+                Logger.getGlobal().log(Level.SEVERE, ex.getMessage(), ex);
+            });
+            loadService = service;
+        }
+        loadService.restart();
     }
 
+    /**
+     * Populate UI with provided item wrappers.
+     * @param wrapper Item wrappers.
+     */
+    private void populateUI(final List<ItemWrapper> wrappers) {
+        final int itemNumber = wrappers.size();
+        gridPane.getRowConstraints().setAll(IntStream.range(0, itemNumber)
+                .mapToObj(index -> {
+                    final RowConstraints rowConstraints = new RowConstraints();
+                    return rowConstraints;
+                })
+                .toArray(RowConstraints[]::new));
+        for (int index = 0; index < itemNumber; index++) {
+            final ItemWrapper wrapper = wrappers.get(index);
+            final Item item = wrapper.getItem();
+            if (item != null) {
+                final URLReference url = item.getIcon();
+                final Image image = url.isPresent() ? ImageCache.INSTANCE.getImage(url.get().toExternalForm()) : null;
+                final ImageView imageView = new ImageView();
+                imageView.setImage(image);
+                GridPane.setConstraints(imageView, 0, index);
+                gridPane.getChildren().add(imageView);
+            }
+            final ItemTooltipRenderer renderer1 = new ItemTooltipRenderer();
+            renderer1.setItem(wrapper);
+            final Tooltip tooltip = ItemTooltipRenderer.asTooltip(wrapper);
+            Tooltip.install(renderer1, tooltip);
+            GridPane.setConstraints(renderer1, 1, index);
+            gridPane.getChildren().add(renderer1);
+        }
+    }
+
+    /**
+     * Load remote test.
+     * @return A {@code List<ItemWrapper>} instance, never {@code null}.
+     * @throws IOException In case of IO error.
+     */
     private List<ItemWrapper> loadRemoteTest() throws IOException {
         List<ItemWrapper> result = Collections.EMPTY_LIST;
-        URL url = getClass().getResource("items.json");
+        URL url = getClass().getResource("items.json"); // NOI18N.
         if (url != null) {
             // Get ids from local objects.
-            int[] ids = JsonpContext.SAX.loadObjectArray(Item.class, url)
+            final int[] ids = JsonpContext.SAX.loadObjectArray(Item.class, url)
                     .stream()
                     .mapToInt(Item::getId)
                     .toArray();
-            result = WebQuery.INSTANCE.queryItems(ids)
+            result = GW2APIClient.create()
+                    .ids(ids)
+                    .endPoint("items") // NOI18N.
+                    .queryArray(Item.class)
                     .stream()
                     .map(item -> new ItemWrapper(item, null))
                     .collect(Collectors.toList());
         }
-        return result;
+        return Collections.unmodifiableList(result);
 
     }
 
+    /**
+     * Load local test.
+     * @return A {@code List<ItemWrapper>} instance, never {@code null}.
+     * @throws IOException In case of IO error.
+     */
     private List<ItemWrapper> loadLocalTest() throws IOException {
         List<ItemWrapper> result = Collections.EMPTY_LIST;
         URL url = getClass().getResource("items.json");
@@ -128,10 +182,11 @@ public final class TestItemTooltipRenderer extends Application {
                     .map(item -> new ItemWrapper(item, null))
                     .collect(Collectors.toList());
         }
-        return result;
+        return Collections.unmodifiableList(result);
     }
 
     /**
+     * Program entry point.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
